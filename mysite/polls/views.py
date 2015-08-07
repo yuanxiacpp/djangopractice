32261a06b863c5acfac673c33c5bb6943f61b5ac
Yuan Xia <yuanx@yuanx-mbp.local>
2015-08-06 21:47:44 -0700

    complete tutorial 3, tested poll page, please be careful not to include polls.urls inside polls/urls.py, this could result in a infinite recursion problem

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,26 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello world, you are at polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+    #template = loader.get_template('polls/index.html')
+    #context = RequestContext(request, {
+    #    'latest_question_list': latest_question_list
+    #})
+
+    return HttpResponse(template.render(context))
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'poll/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %s."
-    return httpResponse(response % question_id)
+    return HttpResponse(response % question_id)
 
 def vote(request, question_id):
     return HttpResponse("You are voting on question %s." % question_id)
