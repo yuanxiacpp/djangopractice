47913057cb9f72a901c6ad64e4b4bcc957521a6d
Yuan Xia <yuanx@yuanx-mbp.local>
2015-08-06 23:51:01 -0700

    complete tutorial 4

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import include, url
-from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^$', views.IndexView.as_view() , name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
